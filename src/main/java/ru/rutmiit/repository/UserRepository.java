f7b9f4b1ad89e3415b6e8895ccb9ef28bf237f23
karalesus <karalesus@gmail.com>
2024-11-04 19:31:44 +0300

    add methods byEmail to UserRepository

--- src/main/java/ru/rutmiit/repository/UserRepository.java
+++ src/main/java/ru/rutmiit/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package ru.rutmiit.repository;
 
 import ru.rutmiit.domain.SessionRegistration;
+import ru.rutmiit.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface UserRepository {
+    Optional<User> findByEmail(String email);
 
+    boolean existsByEmail(String email);
 }
