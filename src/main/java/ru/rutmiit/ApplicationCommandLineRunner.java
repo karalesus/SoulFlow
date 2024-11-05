a314a91e104100e60a867bd1901290dfd3c68206
karalesus <karalesus@gmail.com>
2024-11-05 12:20:17 +0300

    add methods for CLR

--- src/main/java/ru/rutmiit/ApplicationCommandLineRunner.java
+++ src/main/java/ru/rutmiit/ApplicationCommandLineRunner.java
@@ -3,16 +3,27 @@ package ru.rutmiit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import ru.rutmiit.domain.Difficulty;
 import ru.rutmiit.domain.Role;
+import ru.rutmiit.domain.Type;
 import ru.rutmiit.domain.User;
+import ru.rutmiit.dto.InstructorDTO;
+import ru.rutmiit.dto.SessionDTO;
 import ru.rutmiit.dto.UserDTO;
-import ru.rutmiit.exceptions.InvalidUserDataException;
+import ru.rutmiit.exceptions.instructor.InvalidInstructorDataException;
+import ru.rutmiit.exceptions.session.InvalidSessionDataException;
+import ru.rutmiit.exceptions.user.InvalidUserDataException;
 import ru.rutmiit.repository.implementations.*;
+import ru.rutmiit.service.implementations.InstructorServiceImpl;
+import ru.rutmiit.service.implementations.SessionServiceImpl;
 import ru.rutmiit.service.implementations.UserServiceImpl;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @Component
@@ -24,24 +35,35 @@ public class ApplicationCommandLineRunner implements CommandLineRunner {
     private final UserServiceImpl userService;
     private final UserRepositoryImpl userRepository;
     private final RoleRepositoryImpl roleRepository;
-
+    private final TypeRepositoryImpl typeRepository;
+    private final DifficultyRepositoryImpl difficultyRepository;
+    private final InstructorServiceImpl instructorService;
+    private final SessionServiceImpl sessionService;
     @Autowired
-    public ApplicationCommandLineRunner(UserServiceImpl userService, UserRepositoryImpl userRepository, RoleRepositoryImpl roleRepository) {
+    public ApplicationCommandLineRunner(UserServiceImpl userService, UserRepositoryImpl userRepository, RoleRepositoryImpl roleRepository, TypeRepositoryImpl typeRepository, DifficultyRepositoryImpl difficultyRepository, InstructorServiceImpl instructorService, SessionServiceImpl sessionService) {
         this.userService = userService;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.typeRepository = typeRepository;
+        this.difficultyRepository = difficultyRepository;
+        this.instructorService = instructorService;
+        this.sessionService = sessionService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         initRoles();
         initAdmin();
+        initTypes();
+        initDifficulties();
 
         while (true) {
             System.out.println("Выберите опцию:" +
                     "\n1 - добавить участника" +
                     "\n2 - посмотреть участников" +
-                    "\n3 - обновить участника");
+                    "\n3 - обновить участника" +
+                    "\n4 - добавить инструктора"+
+                    "\n5 - добавить занятие");
 
 
             String input = bufferedReader.readLine().toLowerCase();
@@ -56,8 +78,54 @@ public class ApplicationCommandLineRunner implements CommandLineRunner {
                 case "3":
                     this.editUser();
                     break;
+                case "4":
+                    this.addInstructor();
+                    break;
+                case "5":
+                    this.addSession();
+                    break;
             }
             System.out.println("==================================");
+        }
+    }
+
+    private void addSession() throws IOException {
+        // Хатха-йога для спокойствия души,120,пример описания,2024-11-06 12:00,20,1500,начинающий,хатха,Трофимова Анна Владимировна
+        System.out.println("Введите данные занятия в формате: имя,длительность,описание,дата_время (yyyy-MM-dd HH:mm),макс_вместимость,цена,сложность,тип,инструктор");
+        String[] sessionParams = bufferedReader.readLine().split(",");
+
+        try {
+            LocalDateTime dateTime = LocalDateTime.parse(sessionParams[3], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+            SessionDTO sessionDTO = new SessionDTO(
+                    sessionParams[0],
+                    Integer.parseInt(sessionParams[1]),
+                    sessionParams[2],
+                    dateTime, // Дата в формате "yyyy-MM-dd HH:mm"
+                    Integer.parseInt(sessionParams[4]),
+                    Integer.parseInt(sessionParams[5]),
+                    sessionParams[6],
+                    sessionParams[7],
+                    sessionParams[8]
+            );
+            this.sessionService.addSession(sessionDTO);
+            System.out.println("Занятие успешно добавлено!");
+        } catch (DateTimeParseException e) {
+            System.out.println("Ошибка: неверный формат даты. Используйте 'yyyy-MM-dd HH:mm'.");
+        } catch (InvalidSessionDataException e) {
+            System.out.println("Ошибка добавления занятия: " + e.getMessage());
+        }
+    }
+
+
+    private void addInstructor() throws IOException {
+        System.out.println("Введите данные пользователя в таком формате: имя,сертификат,ссылка_на_фото");
+        String[] instructorParams = bufferedReader.readLine().split(",");
+        InstructorDTO instructorDTO = new InstructorDTO(instructorParams[0], instructorParams[1], instructorParams[2]);
+        try {
+            this.instructorService.addInstructor(instructorDTO);
+            System.out.println("Инструктор успешно добавлен!");
+        }catch (InvalidInstructorDataException e) {
+            System.out.println("Ошибка добавления инструктора: " + e.getMessage());
         }
     }
 
@@ -119,5 +187,23 @@ public class ApplicationCommandLineRunner implements CommandLineRunner {
         adminUser.setRole(List.of(adminRole));
 
         userRepository.save(adminUser);
+    }
+    private void initTypes() {
+        List<String> typeNames = List.of("хатха", "медитация", "кундалини", "фитнес", "виньяса", "ваджра");
+
+        for (String typeName : typeNames) {
+            if (!typeRepository.existsByName(typeName)) {
+                typeRepository.save(new Type(typeName));
+            }
+        }
+    }
+    private void initDifficulties() {
+        List<String> difficultyNames = List.of("начинающий", "средний", "продвинутый");
+
+        for (String difficultyName : difficultyNames) {
+            if (!difficultyRepository.existsByName(difficultyName)) {
+                difficultyRepository.save(new Difficulty(difficultyName));
+            }
+        }
     }
 }
