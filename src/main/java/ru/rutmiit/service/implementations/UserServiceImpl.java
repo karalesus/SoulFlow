f95bf8db1d35b0749e80ae8a5fd9fb2b6f437cf2
karalesus <karalesus@gmail.com>
2024-11-05 12:18:33 +0300

    add validationUtil for user

--- src/main/java/ru/rutmiit/service/implementations/UserServiceImpl.java
+++ src/main/java/ru/rutmiit/service/implementations/UserServiceImpl.java
@@ -1,21 +1,20 @@
 package ru.rutmiit.service.implementations;
 
-import org.modelmapper.ModelMapper;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.ModelMap;
 import ru.rutmiit.domain.Role;
 import ru.rutmiit.domain.User;
 import ru.rutmiit.dto.UserDTO;
-import ru.rutmiit.exceptions.InvalidEmailException;
-import ru.rutmiit.exceptions.InvalidPasswordException;
-import ru.rutmiit.exceptions.InvalidUserDataException;
-import ru.rutmiit.exceptions.UserNotFoundException;
-import ru.rutmiit.repository.RoleRepository;
+import ru.rutmiit.exceptions.session.InvalidSessionDataException;
+import ru.rutmiit.exceptions.user.InvalidEmailException;
+import ru.rutmiit.exceptions.user.InvalidUserDataException;
+import ru.rutmiit.exceptions.user.UserNotFoundException;
 import ru.rutmiit.repository.implementations.RoleRepositoryImpl;
 import ru.rutmiit.repository.implementations.UserRepositoryImpl;
 import ru.rutmiit.service.UserService;
-import ru.rutmiit.utils.Mapper;
+import ru.rutmiit.utils.modelMapper.Mapper;
+import ru.rutmiit.utils.validation.ValidationUtil;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,20 +27,25 @@ public class UserServiceImpl implements UserService {
     private final Mapper mapper;
     private RoleRepositoryImpl roleRepository;
     private UserRepositoryImpl userRepository;
+    private ValidationUtil validationUtil;
 
-    public UserServiceImpl(Mapper mapper, RoleRepositoryImpl roleRepository, UserRepositoryImpl userRepository) {
+    public UserServiceImpl(Mapper mapper, RoleRepositoryImpl roleRepository, UserRepositoryImpl userRepository, ValidationUtil validationUtil) {
         this.mapper = mapper;
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.validationUtil = validationUtil;
     }
 
     @Override
     public void register(UserDTO userDTO) {
-//        if (!userDTO.getPassword().equals(userDTO.getConfirmPassword())) {
-//            throw new InvalidPasswordException("Пароли не сходятся");
-//        }
-        if (userDTO == null) {
-            throw new InvalidUserDataException("Введите данные пользователя!");
+        if (!this.validationUtil.isValid(userDTO)) {
+
+            this.validationUtil
+                    .violations(userDTO)
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .forEach(System.out::println);
+            throw new InvalidUserDataException("Данные пользователя введены неверно!");
         }
         boolean existsByEmail = userRepository.existsByEmail(userDTO.getEmail());
 
@@ -70,10 +74,15 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserDTO editUser(UserDTO userDTO) {
-        if (userDTO == null) {
-            throw new InvalidUserDataException("Введите данные");
-        }
+        if (!this.validationUtil.isValid(userDTO)) {
 
+            this.validationUtil
+                    .violations(userDTO)
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .forEach(System.out::println);
+            throw new InvalidSessionDataException("Данные пользователя введены неверно!");
+        }
         User user = userRepository.findByEmail(userDTO.getEmail()).orElseThrow(() ->
                 new InvalidEmailException("Пользователь с email " + userDTO.getEmail() + " не найден.")
         );
