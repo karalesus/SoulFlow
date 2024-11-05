6f31605d88d2f61c352beb0f96f0f8d8276e515e
karalesus <karalesus@gmail.com>
2024-11-05 12:21:30 +0300

    add validationUtil

---
+++ src/main/java/ru/rutmiit/utils/validation/ValidationUtil.java
@@ -0,0 +1,11 @@
+package ru.rutmiit.utils.validation;
+
+import jakarta.validation.ConstraintViolation;
+
+import java.util.Set;
+
+public interface ValidationUtil {
+    <E> boolean isValid(E object);
+
+    <E> Set<ConstraintViolation<E>> violations(E object);
+}
